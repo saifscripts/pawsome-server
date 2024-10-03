5fcd674ec0bb2ef3134ea4e8f09d6c7f818881df
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-10-03 17:29:36 +0600

    build

---
+++ dist/app/modules/post/post.constant.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PostCategories = exports.POST_CATEGORY = void 0;
+exports.POST_CATEGORY = {
+    TIP: 'tip',
+    STORY: 'story',
+};
+exports.PostCategories = ['tip', 'story'];
