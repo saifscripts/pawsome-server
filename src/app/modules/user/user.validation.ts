5a281db0767ae5646f21f7ba0429ac3c4782e9c3
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-10-11 19:14:37 +0600

    add phone field at update profile validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -5,6 +5,12 @@ const updateProfileValidationSchema = z.object({
     body: z.object({
         name: z.string().min(1, 'Name cannot be an empty string').optional(),
         email: z.string().email('Invalid email address').optional(),
+        phone: z
+            .string()
+            .refine((value) => validator.isMobilePhone(value), {
+                message: 'Invalid phone number',
+            })
+            .optional(),
     }),
 });
 
