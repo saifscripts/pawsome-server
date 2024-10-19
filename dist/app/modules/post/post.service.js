06fe6947233f0f1744bb3c4bdfac20154f1eb137
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-10-19 07:46:50 +0600

    build

--- dist/app/modules/post/post.service.js
+++ dist/app/modules/post/post.service.js
@@ -20,6 +20,7 @@ const QueryBuilder_1 = __importDefault(require("../../builders/QueryBuilder"));
 const AppError_1 = __importDefault(require("../../errors/AppError"));
 const user_constant_1 = require("../user/user.constant");
 const user_model_1 = require("../user/user.model");
+const post_constant_1 = require("./post.constant");
 const post_model_1 = require("./post.model");
 const createPostIntoDB = (authorId, payload, featuredImage) => __awaiter(void 0, void 0, void 0, function* () {
     payload.featuredImage = featuredImage === null || featuredImage === void 0 ? void 0 : featuredImage.path;
@@ -61,7 +62,7 @@ const getPostsFromDB = (user, query) => __awaiter(void 0, void 0, void 0, functi
     const isPremiumUser = (user === null || user === void 0 ? void 0 : user.userType) === user_constant_1.USER_TYPE.PREMIUM &&
         ((_a = user === null || user === void 0 ? void 0 : user.subscription) === null || _a === void 0 ? void 0 : _a.endDate) > new Date();
     const postQuery = new QueryBuilder_1.default(post_model_1.Post.find({ isPublished: true }).populate('author'), query)
-        // .search(PostSearchableFields)
+        .search(post_constant_1.PostSearchableFields)
         .filter()
         .sort()
         .paginate()
@@ -74,6 +75,7 @@ const getPostsFromDB = (user, query) => __awaiter(void 0, void 0, void 0, functi
                     _id: post._id,
                     title: post.title,
                     summary: post.summary,
+                    featuredImage: post.featuredImage,
                     upvotes: post.upvotes,
                     downvotes: post.downvotes,
                     author: post.author,
